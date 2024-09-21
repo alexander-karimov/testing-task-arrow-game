58e1c12bedf37a683b1595c6681cc5a16484d965
AlexanderKarimov <alexander.karimov94@yandex.ru>
2024-09-21 20:52:54 +0300

    Подготовить к деплою на GitHub

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/testing-task-arrow-game/",
   plugins: [react()],
   server: {
     open: true,
